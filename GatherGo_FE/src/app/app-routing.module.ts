71cc889ffcec24b3c4707503c58a6a0ed07d9b55
mirunadam4 <83066185+mirunadam@users.noreply.github.com>
2025-12-18 08:28:04 +0200

    Working on photo updates

--- GatherGo_FE/src/app/app-routing.module.ts
+++ GatherGo_FE/src/app/app-routing.module.ts
@@ -1,21 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent }, //redirectTo: '/login', pathMatch: 'full'
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'profile', component: ProfileComponent }
-];
-=======
 
 import {routes} from "./routes";
->>>>>>> 7c13567f3db0c2639d03980ea6b64faca32b46b8
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
