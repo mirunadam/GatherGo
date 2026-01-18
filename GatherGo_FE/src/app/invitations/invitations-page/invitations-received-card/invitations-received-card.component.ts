25587ea415edffcb6bf6aade4e17929fbee2fcbb
mara0509 <tamasilamara@gmail.com>
2026-01-18 20:30:07 +0200

    The Reeceived part front End

--- GatherGo_FE/src/app/invitations/invitations-page/invitations-received-card/invitations-received-card.component.ts
+++ GatherGo_FE/src/app/invitations/invitations-page/invitations-received-card/invitations-received-card.component.ts
@@ -1,29 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForOf, NgIf } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-
-import { InviteDto } from '../../domain/invite.dto';
-import { InviteStatus } from '../../domain/invite-status';
-import { InviteService } from '../../services/invite.service';
-import { LoggedInContextService } from '../../../services/logged-in-context.service';
+import { NgForOf, NgIf } from "@angular/common";
+import { InviteService } from "../../services/invite.service";
+import { LoggedInContextService } from "../../../services/logged-in-context.service";
+import { InviteDto } from "../../domain/invite.dto";
+import { InviteStatus } from "../../domain/invite-status";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-invitations-received-card',
   standalone: true,
-  templateUrl: './invitations-received-card.component.html',
-  styleUrls: ['./invitations-received-card.component.scss'],
   imports: [
     NgForOf,
     NgIf,
     MatButtonModule
-  ]
+  ],
+  templateUrl: './invitations-received-card.component.html',
+  styleUrls: ['./invitations-received-card.component.scss']
 })
 export class InvitationsReceivedCardComponent implements OnInit {
 
-  receivedInvites: InviteDto[] = [];
+  invites: InviteDto[] = [];
   userEmail: string | undefined;
-
-  protected readonly InviteStatus = InviteStatus;
 
   constructor(
     private inviteService: InviteService,
@@ -38,19 +35,21 @@ export class InvitationsReceivedCardComponent implements OnInit {
         this.inviteService
           .getAllInvitesByReceiverEmail(this.userEmail)
           .subscribe(invites => {
-            this.receivedInvites = invites;
+            this.invites = invites;
           });
       }
     });
   }
 
-  accept(invite: InviteDto) {
+  acceptInvite(invite: InviteDto) {
     invite.status = InviteStatus.ACCEPTED;
     this.inviteService.createInvite(invite).subscribe();
   }
 
-  reject(invite: InviteDto) {
+  rejectInvite(invite: InviteDto) {
     invite.status = InviteStatus.REJECTED;
     this.inviteService.createInvite(invite).subscribe();
   }
+
+  protected readonly InviteStatus = InviteStatus;
 }
